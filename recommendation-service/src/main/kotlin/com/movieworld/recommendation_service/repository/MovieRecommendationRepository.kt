69b6dd6c23606570a2d86d406b0da90c15cf89e9
Arman I. <88910492+Kotik112@users.noreply.github.com>
2025-02-09 04:27:15 +0000

    Progress on integrating the microservices

--- recommendation-service/src/main/kotlin/com/movieworld/recommendation_service/repository/MovieRecommendationRepository.kt
+++ recommendation-service/src/main/kotlin/com/movieworld/recommendation_service/repository/MovieRecommendationRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MovieRecommendationRepository: JpaRepository<MovieRecommendation, Long> {
+interface MovieRecommendationRepository: JpaRepository<Unit, Long> {
 }
